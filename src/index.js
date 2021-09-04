a94a252e3f35c823f9d76990046e1ab1479ef06e
Danny L <danny.95.dl@gmail.com>
2021-09-05 00:43:31 +0200

    added todo-list app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
-import App from './App';
+import App from './App'; //(task-notes and morning and night picture)
+//import App from './App2' (task-fotball cards)
 //import reportWebVitals from './reportWebVitals';
 //import MyInfo from "./MyInfo";
-
+//import App from './App3'
+// import App from './App4'
 
 
 
